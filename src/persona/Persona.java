97affb4a6b52cc42c5519c4815c730d0c4231b70
Mañana <Maana@DESKTOP-T6B1QGE>
2024-04-25 11:45:53 +0200

    Creado el bean Persona con comentarios

---
+++ src/persona/Persona.java
@@ -0,0 +1,104 @@
+package persona;
+
+/**
+ * clase persona
+ */
+public class Persona {
+
+	
+	private String Nombre;
+	private int edad;
+	private float peso;
+	private float altura;
+	
+	
+	/**
+	 * constructor persona
+	 * @param nombre
+	 * @param edad
+	 * @param peso
+	 * @param altura
+	 */
+	public Persona(String nombre, int edad, float peso, float altura) {
+		super();
+		Nombre = nombre;
+		this.edad = edad;
+		this.peso = peso;
+		this.altura = altura;
+	}
+	
+	/**
+	 * getter del nombre
+	 * @return el nombre de la persona
+	 */
+	public String getNombre() {
+		return Nombre;
+	}
+
+	/**
+	 * setter del nombre
+	 * @param nombre
+	 */
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	/**
+	 * getter de la edad
+	 * @return la edad de la persona
+	 */
+	public int getEdad() {
+		return edad;
+	}
+
+	/**
+	 * setter de la edad
+	 * @param edad
+	 */
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	/**
+	 * getter del peso
+	 * @return el peso de la persona
+	 */
+	public float getPeso() {
+		return peso;
+	}
+
+	/**
+	 * setter del peso
+	 * @param peso
+	 */
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+
+	/**
+	 * getter de la altura
+	 * @return la altura de la persona
+	 */
+	public float getAltura() {
+		return altura;
+	}
+
+	/**
+	 * setter de la altura
+	 * @param altura
+	 */
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+
+
+	/**
+	 * toString con el nombre, edad, peso y altura de la persona
+	 */
+	@Override
+	public String toString() {
+		return "Persona [Nombre=" + Nombre + ", edad=" + edad + ", peso=" + peso + ", altura=" + altura + "]";
+	}
+	
+	
+}
