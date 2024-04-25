1981f9b0ebd491c3a7f32af5fb075594eecfc711
Mañana <Maana@DESKTOP-T6B1QGE>
2024-04-25 12:34:48 +0200

    Creada la clase PersonaData con laas restricciones y actualizada la clase ClonadorPersona con el exception

---
+++ src/clonador/ClonadorPersona.java
@@ -0,0 +1,41 @@
+package clonador;
+
+import persona.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class ClonadorPersona {
+
+	
+	private String nombre;
+
+	
+	public ClonadorPersona(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	public List<Persona> clonarPersonas() {
+		List<Persona> personas = new ArrayList<Persona>();
+	    
+		for (int i=0; i<10; i++) {
+			try {
+				int edad = (int) (Math.random()*PersonaData.EDAD_MAX + PersonaData.EDAD_MIN);
+				double peso = (double) (Math.random()*PersonaData.PESO_MAX + PersonaData.PESO_MIN);
+				double altura = (double) (Math.random()*PersonaData.ALTURA_MAX + PersonaData.ALTURA_MIN);
+				Persona newPersona = new Persona(nombre, edad, peso, altura);
+				personas.add(newPersona);
+			}
+			catch (Exception e){
+				System.err.println("Error al crear el clon" + e.getMessage());
+			}
+
+		}
+		
+		return personas;  
+	}
+	
+	
+	
+	
+}
---
+++ src/persona/PersonaData.java
@@ -0,0 +1,15 @@
+package persona;
+
+public class PersonaData {
+
+	
+	public static final double PESO_MAX = 100;
+	public static final double ALTURA_MAX = 200;
+	public static final int EDAD_MAX = 99;
+	
+	public static final double PESO_MIN = 0;
+	public static final double ALTURA_MIN = 0;
+	public static final int EDAD_MIN = 0;
+	
+	
+}
